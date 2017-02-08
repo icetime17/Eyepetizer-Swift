0fd53b21e470d958ffef5fd441338c57e05132d1
Chris Hu <icetime17@126.com>
2017-02-08 11:40:21 +0800

    Hide status bar when launching, then show status bar on view controller

--- Eyepetizer-Swift/Eyepetizer-Swift/AppDelegate.swift
+++ Eyepetizer-Swift/Eyepetizer-Swift/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // use to show status bar on ViewController
+        application.setStatusBarHidden(false, with: .slide)
+        
         return true
     }
 
